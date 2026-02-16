71553479db95a7bb8753aecf04d075dd484a08d3
sprynar <sprynar@zohomail.com>
2026-02-16 14:45:24 +0100

    initial

---
+++ src/main/java/pro1/NumericUtils.java
@@ -0,0 +1,12 @@
+package pro1;
+
+public class NumericUtils {
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return Math.abs(a);
+    }
+}
