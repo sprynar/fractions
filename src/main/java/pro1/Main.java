82e19f9214e724996941eb44a1c5c2b0591ff692
sprynar <sprynar@zohomail.com>
2026-02-16 16:18:30 +0100

    přecházím na domácí úkol

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -1,8 +1,37 @@
 package pro1;
-
+import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("ahoj krásný světe");
+        Path dirPath = Paths.get("input");
+
+        if (!Files.exists(dirPath)) {
+            System.err.println("Directory does not exist: " + dirPath);
+            return;
+        }
+
+        try (Stream<Path> paths = Files.walk(dirPath)) {
+            paths.filter(Files::isRegularFile)
+                    .filter(p -> p.toString().endsWith(".csv"))
+                    .forEach(path -> {
+                        try {
+                            Files.lines(path).forEach(line -> {
+                                String[] parts = line.split("[,;\t=:]");
+                                System.out.println(parts[0]);
+                                System.out.print("; "+ parts[1] +"; ");
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/pro1/FractionTest1.java
+++ src/test/java/pro1/FractionTest1.java
@@ -7,5 +7,6 @@ public class FractionTest1
     void test()
     {
         Assertions.assertEquals("12/25",new Fraction(72,150).vratZlomek());
+
     }
 }
