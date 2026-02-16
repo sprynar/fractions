69ca3ed2f649cf0be8717627e30087f67e524f03
sprynar <sprynar@zohomail.com>
2026-02-16 15:58:41 +0100

    přecházím na domácí úkol

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -1,5 +1,8 @@
 package pro1;
 
-public class Main {
 
+public class Main {
+    public static void main(String[] args){
+        System.out.println("ahoj krásný světe");
+    }
 }
--- src/main/java/pro1/NumericUtils.java
+++ src/main/java/pro1/NumericUtils.java
@@ -4,6 +4,7 @@ public class NumericUtils {
     public static long gcd(long a, long b) {
         while (b != 0) {
             long tmp = a % b;
+            System.out.println(a + ";" + b);
             a = b;
             b = tmp;
         }
